78b7ea1f7f9300bf190434eaad34605f91050bc4
1625081 <sanjin@buaa.edu.cn>
2018-03-15 01:35:46 +0800

    add_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 原key:为什么输入法显示不出来
+articles = Article.create(
+  [
+    {title: "输入法语言栏不显示 - 搜狗输入法"},
+    {title: "输入法不见了（不能切换）怎么办？_百度经验"},
+    {title: "电脑输入法显示不出来怎么办_百度知道"},
+    {title: "为什么输入法出不来"},
+    {title: "为什么QQ拼音输入法的界面显示不出来啊"},
+    {title: "为什么我安装的输入法总是不显示出来?_百度知道"},
+    {title: "win7输入法显示不出来怎么办_百度经验"},
+    {title: "输入法显示不出来的解决办法"},
+    {title: "搜狗拼音输入法为什么在桌面上显示不出来?_百度知道"},
+    {title: "为什么电脑显示不出输入法图标?_百度知道"},
+    {title: "输入法的键盘显示不见了是怎么回事?_百度知道"}
+  ]
+)
